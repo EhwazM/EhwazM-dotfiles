6ebb90916be651f45d0c1a772a28c8eff1e857b8
EhwazM <127918270+EhwazM@users.noreply.github.com>
2024-08-07 18:24:43 -0500

    Hola

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -25,10 +25,10 @@ return {
 		dependencies = {
 			{
 				"rcarriga/nvim-dap-ui",
-        dependencies = {
-          "folke/neodev.nvim",
-          "nvim-neotest/nvim-nio",
-         },
+				dependencies = {
+					"folke/neodev.nvim",
+					"nvim-neotest/nvim-nio",
+				},
 				config = function()
 					require("neodev").setup({
 						library = { plugins = { "nvim-dap-ui" }, types = true },
@@ -52,17 +52,17 @@ return {
 			local dapPython = require("dap-python")
 
 			dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 
 			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
 			vim.keymap.set("n", "<leader>dc", dap.continue, {})
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,27 +1,27 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
 
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = false, -- when true, they will just be displayed differently than normal items
-					hide_dotfiles = false,
-					hide_gitignored = true,
-					hide_hidden = false, -- only works on Windows for hidden files/directories
-					hide_by_name = {
-						".DS_Store",
-						"thumbs.db",
-					},
-				},
-			},
-		})
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = false, -- when true, they will just be displayed differently than normal items
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_hidden = false, -- only works on Windows for hidden files/directories
+                    hide_by_name = {
+                        ".DS_Store",
+                        "thumbs.db",
+                    },
+                },
+            },
+        })
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    end,
 }
